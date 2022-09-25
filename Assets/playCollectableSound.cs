e1e69e4ae1676abc7209fc11493c529abdf86c20
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2022-09-25 18:21:25 +0200

    little changes

--- Assets/playCollectableSound.cs
+++ Assets/playCollectableSound.cs
@@ -5,11 +5,11 @@ using UnityEngine.Audio;
 
 public class playCollectableSound : MonoBehaviour{
     
-    public AudioSource clip;
+    public AudioSource collectableClip;
 
     void OnTriggerEnter(Collider other){
         if(other.tag == "Collectable"){
-            clip.Play();
+            collectableClip.Play();
         }
     }
 }
