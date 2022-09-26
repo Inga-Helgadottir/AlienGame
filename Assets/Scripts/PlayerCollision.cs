3f0dd6e4bf2715d590c6ff02470067c29dd73846
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2022-09-26 11:09:34 +0200

    finishing toutches

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine; 
+using UnityEngine.Audio;
 
 public class PlayerCollision : MonoBehaviour{
     
     public int playerHealth;
+    public AudioSource alienCrashSound;
     [SerializeField] GameManager gameManager;
 
     void awake(){
@@ -14,6 +16,7 @@ public class PlayerCollision : MonoBehaviour{
     void OnTriggerEnter(Collider collider){
         if(collider.tag == "Enemy"){
             playerHealth -= 10;
+            alienCrashSound.Play();
             if(playerHealth == 0){
                 gameManager.EndGame(false);
             }
