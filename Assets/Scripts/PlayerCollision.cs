40155ba733b0253519c5991bf45ac8f2fb3c95fb
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2022-12-27 15:51:57 +0100

    now the health helper adds to the players health score

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine; 
 using UnityEngine.Audio;
 using TMPro;
+using System;
+using Random = UnityEngine.Random;
 
 public class PlayerCollision : MonoBehaviour{
     
@@ -11,6 +13,8 @@ public class PlayerCollision : MonoBehaviour{
     public AudioSource alienCrashSound;
     [SerializeField] GameManager gameManager;
     public CameraShake cameraShake;
+    public System.Random ran = new System.Random();
+    public GameObject healthHelper;
 
     void awake(){
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
@@ -18,17 +22,38 @@ public class PlayerCollision : MonoBehaviour{
     }
     
     void OnTriggerEnter(Collider collider){
-        Debug.Log("collider");
-        Debug.Log(collider);
-        Debug.Log(collider.tag);
         if(collider.tag == "Enemy"){
-            playerHealth -= 10;
-            healthText.text = "Health: " + playerHealth;
+            updateHealth("down");
             alienCrashSound.Play();
             StartCoroutine(cameraShake.Shake(.1f, .1f));
             if(playerHealth == 0){
                 gameManager.EndGame(false);
             }
+        }else if(collider.tag == "HealthHelper"){
+            handleHealthHelperCollision();
         }
+    }
+
+    public void updateHealth(string upOrDown){
+        if(upOrDown == "down"){
+            playerHealth -= 10;
+        }else{
+            playerHealth += 10;
+        }
+        healthText.text = "Health: " + playerHealth;
+    }
+
+    public void handleHealthHelperCollision(){
+        int xMin = 777;
+        int xMax = 1715;
+        
+        int zMin = 105;
+        int zMax = 1475;
+
+        int x = ran.Next(xMin, xMax); 
+        int z = ran.Next(zMin, zMax); 
+
+        healthHelper.transform.position = new Vector3((float)x, 1.6f, (float)z);
+        updateHealth("up");
     }
 }
