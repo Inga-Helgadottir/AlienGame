0a1af7fef03185a34dc273ad4151e90c964d25bb
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2023-01-04 14:09:06 +0100

    added a health bar to my aliens

--- Assets/Scripts/Shootable.cs
+++ Assets/Scripts/Shootable.cs
@@ -2,16 +2,23 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Shootable : MonoBehaviour{
 
+    [SerializeField] Image _healthBarSprite;
     public int currentHealth = 3;
     
     public void Damage(int damageAmount){
         currentHealth-= damageAmount;
-
+        UpdateHealthBar(3);
         if(currentHealth <= 0){
             gameObject.SetActive(false);
         }
     }
+    
+    public void UpdateHealthBar(float maxHealth){
+        _healthBarSprite.fillAmount = currentHealth / maxHealth;
+    }
+
 }
